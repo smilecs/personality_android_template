224aa20f46f60bf6c20c1a0f20603496be529458
egbai mmumene <mumene@gmail.com>
2020-05-29 13:52:01 +0300

    added mock to viewmodel

--- presentation/src/main/java/com/smile/presentation/MainViewModel.kt
+++ presentation/src/main/java/com/smile/presentation/MainViewModel.kt
@@ -1,8 +1,14 @@
 package com.smile.presentation
 
+import com.smile.domain.entities.DemoModel
+import com.smile.domain.usecase.DemoUseCase
 import com.smile.presentation.base.BaseViewModel
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor() : BaseViewModel() {
+class MainViewModel @Inject constructor(private val demoUseCase: DemoUseCase) : BaseViewModel() {
+
+    fun demo(): DemoModel {
+        return demoUseCase()
+    }
 
 }
